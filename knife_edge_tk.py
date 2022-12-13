d15d7c34ff7e1ffb2ab32bb971d5a6d71c4a5b6d
K.Y.Chen <snail32706@gmail.com>
2022-12-13 20:35:44 +0800

    down only one global variable

--- knife_edge_tk.py
+++ knife_edge_tk.py
@@ -12,15 +12,53 @@ from scipy.special import erf
 from scipy.optimize import curve_fit
 from scipy.signal import find_peaks
 
-# file_path_bytk = None
-xdat_row, ydat_row = None, None
+# ------ --------- ------ #
+# ------ Load File ------ #
+
+def load_path():
+    root = tk.Tk()
+    root.withdraw()
+    folder_path = filedialog.askdirectory()
+    return folder_path
+
+def r_all_file():
+    '''
+    get all .txt file.
+    then write in a list: [file name, s=?, z=?, ...]
+    '''
+    folder_path = load_path()
+
+    list_of_file = []
+    # os.chdir("/Users/")
+    os.chdir(folder_path)
+    
+    for file in glob.glob("*.txt"):
+        if judgment_format(file) is not None:
+            list_of_file.append(  judgment_format(file)[0]  )
+    '''
+    list_of_file: [ 'file_name', 'file_name', 'file_name', ... ]
+    '''
+    return list_of_file, folder_path 
+
+def judgment_format(file):
+    '''
+    split flie name by '_'
+    [ file_name, speed, z ]  >> type: str, int, int
+    '''
+    file_name = str(file)
+    if file_name[0] == 's':
+        SS, ZZ, _ = file_name.split('_')
+        _ , speed = SS.split('s')
+        _ , z_component = ZZ.split('z')
+        return file_name, int(speed), int(z_component)
 
 def open_file(): 
     '''
+    使用相對路徑
     *** load data is the first step. ***
     use tkinter
     '''
-    global speed, z_component
+
     filetypes = ( ('text files', '*.txt'), ('All files', '*.*') )
     # global file_path_bytk
     file = tk.filedialog.askopenfile(
@@ -32,47 +70,27 @@ def open_file():
     if file:
         file_path_bytk = os.path.abspath(file.name)
         # file_path_bytk = str(file_path_bytk)
-        # type(file_path_bytk) is str
-
-    speed, z_component = found_name(file)
-
-    rawdat = np.loadtxt(file_path_bytk, delimiter='\t')  
-    xdat = rawdat[:,0]  
-    ydat = rawdat[:,1]  
     
-    return xdat, ydat, speed, z_component
+    return file_path_bytk
 
-def found_name(file):
-    '''
-    Use split to found file name.
-    than get the speed & z component.
-    '''
-    file_name_all = str(file)
+def absolute_to_relative(absolute_file_path):
 
-    for i in file_name_all.split('/'):
+    file_name = str(absolute_file_path)
+    for i in file_name.split('/'):
         if 'txt' in i:
-            file_name, _ = i.split('txt')
-            
-    if file_name[0] != 's':
-        # print('Name Error! \nFile name should be: \n\ts100_z10_ ...')
-        return None, None
-    else:
-        for i, word in enumerate( file_name.split('_') ):
-            if i == 0 and word[0] == 's':
-                _, speed_local = word.split('s')
-            elif i == 1 and word[0] == 'z':
-                _, z_component_local = word.split('z')
-        return int(speed_local), int(z_component_local)
+            return i
 
 def read_file(file_name):
-    
+    '''
+    使用絕對路徑
+    '''
     rawdat = np.loadtxt(file_name, delimiter='\t')  
-
     xdat = rawdat[:,0]
     ydat = rawdat[:,1]
-    
     return xdat, ydat
-# xdat_row, ydat_row = read_file(file_path_bytk)
+
+# ------ -------- ------ #
+# ------ def func ------ #
 
 def myerf(x, a, k, x0, y0):
     
@@ -100,7 +118,7 @@ def plt_xy(x, y, A, axis=None):
     elif A == '.-':
         ax1.plot(x, y, 'b.-')
 
-def fit_error(xdat, ydat, mid_x):
+def fit_error(xdat, ydat, mid_x, mid_y):
     
     '''
     2a: delta of f(-inf) to f(inf)
@@ -108,14 +126,46 @@ def fit_error(xdat, ydat, mid_x):
     x0: mid of y 對應的 x (func 中心)
     c : mid of y
     '''
-    p0=[0.023, 80.0, mid_x, data_list[0]]
+
+    p0=[0.023, 80.0, mid_x, mid_y]
     popt, pcov = curve_fit(myerf, xdat, ydat, p0, method="lm")
     perr = np.sqrt(np.diag(pcov)) * 4.0
     σ = np.sqrt(np.diag(pcov))        
     a = pd.DataFrame(data={'params':popt,'σ':σ}, index = myerf.__code__.co_varnames[1:])      
-    # display(a)   
-
+    # display(a) 
     return popt
+
+def calculate_spot_size(k_parameters, speed):
+    '''
+    put all k
+    calculate spot size and StD.
+
+    parameter k is slope which mean k = Δy / Δx.
+    If Δx = time(s) * speed(um/s). 
+    Slope k(1/um) = k(1/s) * (1/speed)
+    '''
+    spot_size_list = [] # unit: um^2
+    for i, val in enumerate(k_parameters):
+        k_um = val / speed 
+        spot_size = 2*np.pi / ( k_um**2 ) # spot size = pi * r^2
+        spot_size_list.append(spot_size)
+    return spot_size_list 
+
+# ------ ------------ ------ #
+# ------ Data Process ------ #
+
+def find_midy(xdat, ydat):
+    '''
+    Use distribution 
+    first : up or bottem data
+    second: up or bottem data
+    '''
+    counts, dis = np.histogram(ydat, bins= 5)  
+    # plt.stairs(counts, dis) # 可刪
+    max1 = counts.tolist().index(sorted(counts, reverse = True)[0])
+    max2 = counts.tolist().index(sorted(counts, reverse = True)[1])
+    mid_y = (dis[max1] + dis[max2]) / 2
+    return mid_y
 
 def skip_noise(xdat, ydat):
     
@@ -128,7 +178,6 @@ def skip_noise(xdat, ydat):
     '''
 
     # First step:
-    
     counts, dis = np.histogram(ydat, bins= 10)    
     _y = counts.tolist()
     max_index = _y.index(max(counts)) 
@@ -137,46 +186,21 @@ def skip_noise(xdat, ydat):
     noise_top = dis[max_index] + delta/2
     noise_bottom = dis[max_index] - delta/2
     
-    '''
-    Use distribution 
-    first : noise
-    second: up or bottem data
-    third : up or bottem data
-    '''
-    
-    max1 = counts.tolist().index(sorted(counts, reverse = True)[1])
-    max2 = counts.tolist().index(sorted(counts, reverse = True)[2])
-    mid_y = (dis[max1] + dis[max2]) / 2
-    
-    # define the up data adn below data
-    up_data, down_data, data_list = float(0), float(0), []
-    if dis[max2] > dis[max1]:
-        up_data = dis[max2]
-        down_data = dis[max1]
-    else:
-        up_data = dis[max1]
-        down_data = dis[max2]
-    data_list = [mid_y, up_data, down_data]
-
     # Second step:
-
     '''
     noise might be two line.
     i need to skip all of it.
     '''
-      
     xx, yy = [], []
     yyy = sorted(  ydat.tolist()  )
-    
     for i, val_y in enumerate(ydat):
         if val_y > yyy[counts[0]]:
             xx.append(xdat[i])
             yy.append(ydat[i]) 
-    return xx, yy, data_list  
-# x_new, y_new, data_list = skip_noise(xdat_row ,ydat_row)
+    return xx, yy
+
 
 def gradient(x, y, N):
-    
     '''
     average every N points.
 
@@ -186,39 +210,33 @@ def gradient(x, y, N):
     First, the data is averaged every N points.
     Then, useing gradient method find the throld point.
     '''
-
     ave_x, ave_y, y_grad, y_grad_ = [], [], [], []
     for i in range(0,len(y)- N, N):
         total = 0.0
         i = int(i)
-        
         for j in range(N-1):
             total += y[i+j]
         ave_y.append(total / N)
         if i == 0:
             continue   # pass first, beacause of gradient.
         ave_x.append(x[i])
-
+    ave_x = ave_x[0:len(ave_x)-1]
 
     # gradient data
     for i in range(len(ave_y)-2):
         y = (ave_y[i+2] - ave_y[i]) / 2    
         y_grad.append(y)
-        y_grad_.append(abs(y))
-    ave_x = ave_x[0:len(ave_x)-1]
 
-    return ave_x ,y_grad, y_grad_
+    avg_x_byN = np.array(ave_x) # list 不方便計算
+    del_y_byN = np.array(y_grad)
+    return avg_x_byN, del_y_byN, N
 
 def Find_thepeak(x_new, y_new, N):
-    
-    ave_x, y_grad, y_grad_= gradient(x_new, y_new, N)
-    
-    counts, dis = np.histogram(y_grad, bins= 5)
-    # plt.stairs(counts, dis) # 可刪
-    delta = abs(dis[0] - dis[2])
 
-    peaks, _ = find_peaks(y_grad_, height=delta)
-    
+    avg_x_byN, del_y_byN = gradient(x_new, y_new, N)[0], gradient(x_new, y_new, N)[1]
+    counts, dis = np.histogram(del_y_byN, bins= 5)
+    delta = abs(dis[0] - dis[2])
+    peaks, _ = find_peaks(abs(del_y_byN), height=delta)
     # '''
     # find peak 找到鄰近的值，需要移除
     # '''
@@ -228,12 +246,10 @@ def Find_thepeak(x_new, y_new, N):
                 pass
             elif abs(val - peaks[i-1]) < N*20 :
                 peaks = np.delete(peaks, i)
-
     x, y = [], []   
     for i in peaks:
         x.append(x_new[i*N+3])   
         y.append(y_new[i*N+3])
-        
     x_period_pixel = None
     if len(peaks) == 0:
         print('Error at find peaks! There is no peaks')
@@ -248,26 +264,23 @@ def Find_thepeak(x_new, y_new, N):
             x_period_pixel = x_period_2
         else:
             x_period_pixel = x_period_1      
-    
     peak_end = peaks[len(peaks)-1]
     if x_period_pixel != None:
-        if len(y_grad) - peak_end < x_period_pixel/10:
+        if len(del_y_byN) - peak_end < x_period_pixel/10:
             np.delete(peaks, len(peaks)-1)
         if peaks[0] < x_period_pixel/10:
             np.delete(peaks, 0)
+    return x, peaks
 
-    return x, peaks, ave_x, y_grad
-
-def data_split_and_fit(N):
-    
-    mid_ofx, xpeaks, ave_x, y_grad = Find_thepeak(x_new, y_new, N)
-    
+def data_split_and_fit(x_new, y_new, xpeaks, N):
+    '''
+    D_1, D_2 分別裝切割後的 diff error func region of data.
+    N : avg of N number data.
+    '''
     D_1, D_2 = [], []
     if len(xpeaks) == 1:
-        '''
-        D_1 = x_new 
-        '''
-        pass  
+        D_1 = x_new
+        D_2 = y_new
     else:
         for i, xval in enumerate(xpeaks):
             if i == 0:
@@ -289,38 +302,101 @@ def data_split_and_fit(N):
                 y = y_new[peak_mid*N:]
                 D_1.append(x)
                 D_2.append(y)
-    
-    return D_1, D_2, mid_ofx, xpeaks, ave_x, y_grad
-# D_1, D_2, mid_ofx, xpeaks, ave_x, y_grad = data_split_and_fit(3) # 改變平均值
+    return D_1, D_2
 
-spot_size_list = []
-def calculate_spot_size(k_parameter):
+def append_all_k(D_1, D_2, mid_ofx, mid_y):
     '''
-    put all k
-    calculate spot size and StD.
+    fit error func and append all k.
     '''
-    for i, val in enumerate(k_parameter):
-        k_um = val / speed 
-        spot_size = 2*np.pi / ( k_um**2 ) # spot size = pi * r^2
-        spot_size_list.append(spot_size)
+    k_parameter = []
+    if len(mid_ofx) == 0:
+        print('ERROR mid_ofx is None')
+    elif len(mid_ofx) == 1:
+        p = fit_error(D_1, D_2, mid_ofx[0], mid_y)
+        k_parameter.append(p[1])
+    else:    
+        for i, val in enumerate(mid_ofx):
+            p = fit_error(D_1[i], D_2[i], val, mid_y)
+            k_parameter.append(p[1])
+    return k_parameter
 
-'''
-kife_edge.py end
+def create_z_list(list_of_file):
+    '''
+    list_of_file: [ 'file_name', 'file_name', 'file_name', ... ]
+    '''
+    z_list = []
+    for i in list_of_file:
+        if judgment_format(i)[2] not in z_list:
+            z_list.append( judgment_format(i)[2]
+                                                )
+    return sorted(z_list, reverse = True) # z list 由大到小排列
 
-'''
-speed, z_component = None, None
-x_new, y_new, data_list = None, None, None
-D_1, D_2, mid_ofx, xpeaks, ave_x, y_grad = None, None, None, None, None, None
+def main():
+
+    '''
+    Open folder then load all .txt file
+    '''
+
+    spot_dependence_on_z = [] # 總檔案 list
+    chack_file_list = []
+
+    list_of_file, folder_path = r_all_file()
+    z_list = create_z_list(list_of_file) # 由大到小
+    '''
+    z 從最大值 開始讀檔
+    '''
+    for i, ZZ in enumerate(z_list):
+        spot_dependence_on_z.append([ZZ])
+        chack_file_list.append([ZZ])
+
+        for f_name in list_of_file:  
+            
+            file_name, speed, z_int = judgment_format(f_name)
+            absolute_file_path = f'{folder_path}/{file_name}' # 絕對路徑
+
+            if z_int == ZZ:
+                '''
+                if z the same. Begine 
+                '''
+                chack_file_list[i].append(file_name)
+
+                xdat_r, ydat_r          = read_file(absolute_file_path)
+
+                x_new, y_new            = skip_noise(xdat_r, ydat_r)
+                avg_x_byN, del_y_byN, N = gradient(x_new, y_new, 3)
+                mid_ofx, xpeaks         = Find_thepeak(x_new, y_new, N) # 與 gradient 綁在一起
+                D_1, D_2                = data_split_and_fit(x_new, y_new, xpeaks, N) # 與 Find_thepeak 綁在一起
+
+                k_parameters   = append_all_k(D_1, D_2, mid_ofx, find_midy(x_new, y_new))
+                spot_size_list = calculate_spot_size(k_parameters, speed)
+
+                for spot_i in spot_size_list:
+                    spot_dependence_on_z[i].append( round(spot_i, 2) )
+    return spot_dependence_on_z
+
+# ------ ------------ ------ # 
+# ------ tkinter func ------ # 
+
+absolute_file_path = None # 唯一 global variable
 
 def B0f():
     '''
     load row data and show
     '''
-    global xdat_row, ydat_row, x_new, y_new, data_list, D_1, D_2, mid_ofx, xpeaks, ave_x, y_grad, speed
+    global absolute_file_path
 
-    xdat_row, ydat_row, speed, z_component = open_file()
-    x_new, y_new, data_list = skip_noise(xdat_row, ydat_row)    
-    D_1, D_2, mid_ofx, xpeaks, ave_x, y_grad = data_split_and_fit(3) # 改變平均值
+    
+
+    absolute_file_path = open_file()
+    file_name = absolute_to_relative(absolute_file_path)
+    _, speed, z_int = judgment_format(file_name)
+
+    xdat_row, ydat_row      = read_file(absolute_file_path)
+    x_new, y_new            = skip_noise(xdat_row, ydat_row)
+    avg_x_byN, del_y_byN, N = gradient(x_new, y_new, 3)
+    mid_ofx, xpeaks         = Find_thepeak(x_new, y_new, N) # 與 gradient 綁在一起
+    D_1, D_2                = data_split_and_fit(x_new, y_new, xpeaks, N) # 與 Find_thepeak 綁在一起
+
     ax.clear()
     ax.set_xlabel("time (s)")
     ax.set_ylabel("signal (a.u.)")
@@ -336,18 +412,21 @@ def B0f():
         text = f'Load Success!')
         l.place(relx=0.02, rely=0.85, relwidth=0.16, relheight=0.05)
 
-    elif speed is None:
-        var = tk.StringVar()
-        errre_for_filename = tk.Label(root, textvariable=var, bg='#C6C6C6', fg='#F00000', font=("Arial", 18))
-        var.set('Name Error! \nFile name should be: \n\ts100_z10_ ...')
-        errre_for_filename .place(relx=0.26, rely=0.81)
-
-
 def B1f():
     '''
     show processing data
     '''
-    # x_new, y_new, data_list = skip_noise(xdat_row, ydat_row)    
+    # x_new, y_new, data_list = skip_noise(xdat_row, ydat_row)   
+
+    file_name = absolute_to_relative(absolute_file_path)
+    _, speed, z_int = judgment_format(file_name)
+
+    xdat_row, ydat_row      = read_file(absolute_file_path)
+    x_new, y_new            = skip_noise(xdat_row, ydat_row)
+    avg_x_byN, del_y_byN, N = gradient(x_new, y_new, 3)
+    mid_ofx, xpeaks         = Find_thepeak(x_new, y_new, N) 
+    D_1, D_2                = data_split_and_fit(x_new, y_new, xpeaks, N) 
+
     ax.clear()
     ax.set_xlabel("time (s)")
     ax.set_ylabel("signal (a.u.)")
@@ -358,20 +437,40 @@ def B2f():
     '''
     show gradient and peaks.
     '''
+    file_name = absolute_to_relative(absolute_file_path)
+    _, speed, z_int = judgment_format(file_name)
+
+    xdat_row, ydat_row      = read_file(absolute_file_path)
+    x_new, y_new            = skip_noise(xdat_row, ydat_row)
+    avg_x_byN, del_y_byN, N = gradient(x_new, y_new, 3)
+    mid_ofx, xpeaks         = Find_thepeak(x_new, y_new, N) 
+    D_1, D_2                = data_split_and_fit(x_new, y_new, xpeaks, N) 
 
     ax.clear()
     ax.set_xlabel("time (s)")
     ax.set_ylabel("signal (a.u.)")
-    ax.plot(ave_x, y_grad, 'b.-',markersize = 4), ax.grid(True)
+    ax.plot(avg_x_byN, abs(del_y_byN), 'b.-',markersize = 4), ax.grid(True)
     # ax.scatter(x, y, s=area, c=colors, alpha=0.5)
     for i in xpeaks:
-        ax.scatter(ave_x[i], y_grad[i], s=8**2, c='r', alpha=0.5)
+        ax.scatter(avg_x_byN[i], abs(del_y_byN)[i], s=8**2, c='r', alpha=0.5)
     line.draw()    
 
-
 def B3f():
+    
+    '''
+    Fit Error func
+    '''
+    file_name = absolute_to_relative(absolute_file_path)
+    _, speed, z_int = judgment_format(file_name)
+    
+    xdat_row, ydat_row      = read_file(absolute_file_path)
+    x_new, y_new            = skip_noise(xdat_row, ydat_row)
+    avg_x_byN, del_y_byN, N = gradient(x_new, y_new, 3)
+    mid_ofx, xpeaks         = Find_thepeak(x_new, y_new, N) 
+    D_1, D_2                = data_split_and_fit(x_new, y_new, xpeaks, N) 
+    k_parameters            = append_all_k(D_1, D_2, mid_ofx, find_midy(x_new, y_new))
+    spot_size_list = calculate_spot_size(k_parameters, speed)
 
-    k_parameter = []
     ax.clear()
     ax.plot(x_new, y_new, 'b.',markersize = 5)
     ax.set_xlabel("time (s)")
@@ -380,30 +479,23 @@ def B3f():
     c = ['#B22222', '#CD9B1D', '#FF7D40', '#FFC125', '#FF3030', '#FFC125', 
         '#B22222', '#CD9B1D', '#FF7D40', '#FFC125', '#FF3030', '#FFC125'] # color code for fit diff curve
     if len(mid_ofx) == 1:
-            p = fit_error(x_new, y_new, mid_ofx[0])
+            p = fit_error(x_new, y_new, mid_ofx[0], find_midy(x_new, y_new))
             a, k, x0, y0 = p[0], p[1], p[2], p[3]
             ax.plot(x_new, myerf(x_new, a, k, x0, y0), color=c[0], linewidth=8, alpha=0.5)
-            k_parameter.append(p[1])
     else:    
         for i, val in enumerate(mid_ofx):
-            p = fit_error(D_1[i], D_2[i], val)
+            p = fit_error(D_1[i], D_2[i], val, find_midy(x_new, y_new))
             a, k, x0, y0 = p[0], p[1], p[2], p[3]
             ax.plot(D_1[i], myerf(D_1[i], a, k, x0, y0), color=c[i] , linewidth=8, alpha=0.5)
-            k_parameter.append(p[1])
     line.draw()  
 
-    if k_parameter is not None:
-        '''
-        caculate average spot size
+    if k_parameters is not None:
 
-        parameter k is slope which mean k = Δy / Δx.
-        If Δx = time(s) * speed(um/s). 
-        Slope k(1/um) = k(1/s) * (1/speed)
-        '''
-        calculate_spot_size(k_parameter)  # note: spot_size_list is global valuable.
-        spot_size_avg = sum(spot_size_list) / len(spot_size_list)
-        spot_size_StD = np.std(   np.array(spot_size_list)  )
-        spot_size_StD_f = np.round(spot_size_StD, 2)
+        all_spots = np.array(spot_size_list)
+        spot_size_Avg = round( sum(all_spots) / len(all_spots), 2)
+        spot_size_StD = round( np.std(all_spots), 2)
+        N = len(all_spots)
+
 
         var1, var2, var3, var4, var5, var6 = tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar()
         la1 = tk.Label(root, textvariable=var1, bg='#C6C6C6', fg='#000000', font=("Arial", 18))
@@ -416,18 +508,18 @@ def B3f():
         var3.set(speed)
         la3.place(relx=0.32, rely=0.81)
 
-        spot_size_avg_f = round(spot_size_avg, 2)
+
         '''
         Avoid too long values.
         '''
-        if float(spot_size_avg) > 100:
-            spot_size_avg_f = round(spot_size_avg, 1)
-        elif float(spot_size_avg) > 1000:
-            spot_size_avg_f = round(spot_size_avg, 0)
+        if float(spot_size_Avg) > 100:
+            spot_size_Avg = round(spot_size_Avg, 1)
+        elif float(spot_size_Avg) > 1000:
+            spot_size_Avg = round(spot_size_Avg, 0)
 
         # print spot size:
         la4 = tk.Label(root, textvariable=var4, bg='#F0F0F0', fg='#BA1515', font=("Arial", 18), relief="ridge") 
-        var4.set(f'{spot_size_avg_f} ± {spot_size_StD_f}')
+        var4.set(f'{spot_size_Avg} ± {spot_size_StD}')
         la4.place(relx=0.32, rely=0.87)
 
         la5 = tk.Label(root, textvariable=var5, bg='#C6C6C6', fg='#000000', font=("Arial", 18))
@@ -438,7 +530,7 @@ def B3f():
         var6.set("um^2")
         la6.place(relx=0.44, rely=0.87)
 
-    # elif k_parameter == None:
+    # elif k_parameters == None:
         r.place(relx=0.2, rely=0.78, relwidth=0.78, relheight=0.2)
 
 def B4f():
